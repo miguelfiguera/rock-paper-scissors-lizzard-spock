6b65565469e5747962e8f4fb12cd6770ce6e05fe
Miguel Q <36214292+miguelfiguera@users.noreply.github.com>
2022-05-24 20:24:04 -0400

    Add files via upload

--- main.rb
+++ main.rb
@@ -1,22 +1,22 @@
-require_relative "game.rb"
-require_relative "text_module.rb"
-require "pry"
+require_relative 'game'
+require_relative 'text_module'
+require 'pry'
 def game
-game = Game.new
-game.game_prep
-game.turns
-game.reset
-go_again
+  game = Game.new
+  game.game_prep
+  game.turns
+  game.reset
+  go_again
 end
 
 def go_again
-    puts "Do you want to play again? Y/N"
-    answer= gets.chomp.downcase
-    if answer== "y"
-        game
-    else
-        puts "Thanks for playing"
-    end
+  puts 'Do you want to play again? Y/N'
+  answer = gets.chomp.downcase
+  if answer == 'y'
+    game
+  else
+    puts 'Thanks for playing'
+  end
 end
 
 game
--- text_module.rb
+++ text_module.rb
@@ -1,17 +1,17 @@
 module Text
-    def greetings_text
-        puts "Greetings Captain."
-        puts "As you can see, The Borg invites you to play Rock-Paper-Scissors-Lizard-Spock"
-        puts "They want to define who gets the earth in this simple game..."
-    end
+  def greetings_text
+    puts 'Greetings Captain.'
+    puts 'As you can see, The Borg invites you to play Rock-Paper-Scissors-Lizard-Spock'
+    puts 'They want to define who gets the earth in this simple game...'
+  end
 
-    def rules_text
-        puts <<-HEREDOC 
+  def rules_text
+    puts <<-HEREDOC
 
         Rules are quite simple, mon ami:
         1) The first one to get 5 points wins.
         2) Only 10 rounds, after that, the player with more points wins.
-        
+
         Your weapons:
         1)Rock crushes scissors and crushes lizzard.
         2)Paper covers rock and disproves spock.
@@ -22,26 +22,25 @@ module Text
         Choose wisely...
 
 
-        HEREDOC
-    end
+    HEREDOC
+  end
 
-    #methods text
+  # methods text
 
-    def answer_text
-        puts "Write your weapon, Captain (rock,paper,scissors,spock,lizzard)."
-    end
+  def answer_text
+    puts 'Write your weapon, Captain (rock,paper,scissors,spock,lizzard).'
+  end
 
-    def both_answers_text
-        puts "Borg have choosen #{@answer_player2} vs #{@player1.name} has choosen #{@answer_player1}."
-    end
+  def both_answers_text
+    puts "Borg have choosen #{@answer_player2} vs #{@player1.name} has choosen #{@answer_player1}."
+  end
 
-    def player_wins_text(player)
-        puts "#{player} wins this round" if player == @player1.name
-        puts "#{player} win this round" if player == @player2.name
-    end
+  def player_wins_text(player)
+    puts "#{player} wins this round" if player == @player1.name
+    puts "#{player} win this round" if player == @player2.name
+  end
 
-    def points_text
-        puts "#{@player2.name}: #{@player2.points} vs #{@player1.name}: #{@player1.points}"
-    end
-
+  def points_text
+    puts "#{@player2.name}: #{@player2.points} vs #{@player1.name}: #{@player1.points}"
+  end
 end
